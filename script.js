590625b5eb8e3fc47d793a23a67b74d9982a6e84
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2018-06-12 20:51:59 +0200

    Add pad0 function

--- script.js
+++ script.js
@@ -24,6 +24,14 @@ format(times) {
         return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
 }
 
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
+
 const stopwatch = new Stopwatch(
 document.querySelector('.stopwatch'));
 
